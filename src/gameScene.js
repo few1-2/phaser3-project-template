cdb625d932764c4d3e20931cf038a5f532bec80d
Thomas Lee <thomaslee9474@gmail.com>
2019-03-02 00:04:39 -0800

    [added] star and sky

--- src/gameScene.js
+++ src/gameScene.js
@@ -2,20 +2,21 @@ import { Scene } from 'phaser';
 
 class GameScene extends Scene {
   preload() {
-    this.load.image('logo', 'assets/logo.png');
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 
+        'assets/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
   }
 
   create() {
-    const logo = this.add.image(400, 150, 'logo');
+    const sky = this.add.image(0, 0, 'sky')
+    sky.setOrigin(0,0)
 
-    this.tweens.add({
-      targets: logo,
-      y: 450,
-      duration: 2000,
-      ease: 'Power2',
-      yoyo: true,
-      loop: -1,
-    });
+    this.add.image(400, 300, 'star');
   }
 }
 
