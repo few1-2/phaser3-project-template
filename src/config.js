0e958fdd45c40d35469184323ac0e8f526603e60
Thomas Lee <thomaslee9474@gmail.com>
2019-03-07 00:53:40 -0800

    [update] config

--- src/config.js
+++ src/config.js
@@ -8,6 +8,13 @@ const config = {
   width: 800,
   height: 600,
   scene: GameScene,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false
+    },
+  },
 };
 
 export { config };
--- src/gameScene.js
+++ src/gameScene.js
@@ -16,7 +16,15 @@ class GameScene extends Scene {
     const sky = this.add.image(0, 0, 'sky')
     sky.setOrigin(0,0)
 
-    this.add.image(400, 300, 'star');
+    this.createPlatforms();
+  }
+
+  createPlatforms() {
+    this.platforms = this.physics.add.staticGroup();
+    this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    this.platforms.create(600, 400, 'ground');
+    this.platforms.create(50, 250, 'ground');
+    this.platforms.create(750, 220, 'ground');
   }
 }
 
