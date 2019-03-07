2659f852c195409ff8d69432d56dfda27afcddf6
Thomas Lee <thomaslee9474@gmail.com>
2019-03-07 04:53:52 -0800

    [added] bouncing bombs

--- src/gameScene.js
+++ src/gameScene.js
@@ -2,12 +2,14 @@ import { Scene } from 'phaser';
 
 class GameScene extends Scene {
   constructor() {
-    super()
+    super('game');
+
     this.score = 0;
+    this.gameOver = false;
   }
 
-  // =================================================
-  // PRELOAD
+  // ==============================================
+  // PRELOADS
   preload() {
     this.load.image('sky', 'assets/sky.png');
     this.load.image('ground', 'assets/platform.png');
@@ -18,21 +20,26 @@ class GameScene extends Scene {
       { frameWidth: 32, frameHeight: 48 });
   }
 
-  // ===============================================
+  // ======================================================
   // CREATE
   create() {
-    const sky = this.add.image(0, 0, 'sky')
-    sky.setOrigin(0,0)
+    const sky = this.add.image(0, 0, 'sky');
+    sky.setOrigin(0, 0);
 
     this.createPlatforms();
     this.createPlayer();
     this.createCursor();
     this.createStars();
+    this.createBombs();
 
     this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    this.gameOverText = this.add.text(400, 300, 'GAME OVER', { fontSize: '64px', fill: '#ce0000' });
+    this.gameOverText.setOrigin(0.5, 0.5);
+    this.gameOverText.visible = false;
   }
 
   createPlatforms() {
+    this.add.image(400, 300, 'sky');
     this.platforms = this.physics.add.staticGroup();
     this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
     this.platforms.create(600, 400, 'ground');
@@ -42,9 +49,9 @@ class GameScene extends Scene {
 
   createPlayer() {
     this.player = this.physics.add.sprite(100, 450, 'dude');
+    this.player.setCircle(15, 1, 17);
     this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
-
     this.physics.add.collider(this.player, this.platforms);
 
     this.anims.create({
@@ -53,13 +60,11 @@ class GameScene extends Scene {
       frameRate: 10,
       repeat: -1,
     });
-
     this.anims.create({
       key: 'turn',
       frames: [{ key: 'dude', frame: 4 }],
       frameRate: 20,
     });
-
     this.anims.create({
       key: 'right',
       frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
@@ -67,6 +72,7 @@ class GameScene extends Scene {
       repeat: -1,
     });
   }
+
 
   createCursor() {
     this.cursors = this.input.keyboard.createCursorKeys();
@@ -81,20 +87,48 @@ class GameScene extends Scene {
 
     this.stars.children.iterate((child) => {
       child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+      child.setCircle(12);
     });
+
     this.physics.add.collider(this.stars, this.platforms);
     this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
   }
 
   collectStar(player, star) {
     star.disableBody(true, true);
-
     this.score += 10;
-    this.scoreText.setText('Score: ' + this.score);
-  }
-  // =================================================
-  // UPDATE
+    this.scoreText.setText(`Score: ${this.score}`);
 
+    if (this.stars.countActive(true) === 0) {
+      this.stars.children.iterate((child) => {
+        child.enableBody(true, child.x, 0, true, true);
+      });
+      const x = (this.player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+      const bomb = this.bombs.create(x, 16, 'bomb');
+      bomb.setCircle(7);
+      bomb.setBounce(1);
+      bomb.setCollideWorldBounds(true);
+      bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    }
+  }
+
+  createBombs() {
+    this.bombs = this.physics.add.group();
+    this.physics.add.collider(this.bombs, this.platforms);
+    this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this);
+  }
+
+  hitBomb(player, bomb) {
+    this.physics.pause();
+    player.setTint(0xff0000);
+    player.anims.play('turn');
+    this.gameOver = true;
+    this.gameOverText.visible = true;
+    this.input.on('pointerdown', () => this.scene.start('preload'));
+  }
+
+  // =============================================================
+  // UPDATE
   update() {
     if (this.cursors.left.isDown) {
       this.player.setVelocityX(-160);
@@ -106,11 +140,11 @@ class GameScene extends Scene {
       this.player.setVelocityX(0);
       this.player.anims.play('turn');
     }
-
     if (this.cursors.up.isDown && this.player.body.touching.down) {
       this.player.setVelocityY(-330);
     }
   }
 }
+
 
 export default GameScene;
