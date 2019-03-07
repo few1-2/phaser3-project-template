1ca7069d90172b15616bd41c9562fc95ae3c76d9
Thomas Lee <thomaslee9474@gmail.com>
2019-03-07 03:56:59 -0800

    [added] player

--- src/gameScene.js
+++ src/gameScene.js
@@ -6,9 +6,9 @@ class GameScene extends Scene {
     this.load.image('ground', 'assets/platform.png');
     this.load.image('star', 'assets/star.png');
     this.load.image('bomb', 'assets/bomb.png');
-    this.load.spritesheet('dude', 
-        'assets/dude.png',
-        { frameWidth: 32, frameHeight: 48 }
+    this.load.spritesheet('dude',
+      'assets/dude.png',
+      { frameWidth: 32, frameHeight: 48 }
     );
   }
 
@@ -17,6 +17,7 @@ class GameScene extends Scene {
     sky.setOrigin(0,0)
 
     this.createPlatforms();
+    this.createPlayer();
   }
 
   createPlatforms() {
@@ -25,6 +26,33 @@ class GameScene extends Scene {
     this.platforms.create(600, 400, 'ground');
     this.platforms.create(50, 250, 'ground');
     this.platforms.create(750, 220, 'ground');
+  }
+
+  createPlayer() {
+    this.player = this.physics.add.sprite(100, 450, 'dude');
+
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: 'turn',
+      frames: [{ key: 'dude', frame: 4 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    });
   }
 }
 
